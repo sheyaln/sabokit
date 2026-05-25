7a16dc5c356e94462491db535cf4be785dff229d
sheyaln <shehab70@gmail.com>
2026-05-25 11:33:31 -0400

    chore(consumer-template): bump refs to v2.15.3

--- consumer-template/modules/stack/base.tf
+++ consumer-template/modules/stack/base.tf
@@ -1,5 +1,5 @@
 module "base" {
-  source = "git::https://github.com/sheyaln/sabokit.git//platform/base/terraform?ref=v2.15.2"
+  source = "git::https://github.com/sheyaln/sabokit.git//platform/base/terraform?ref=v2.15.3"
   providers = {
     scaleway     = scaleway
     scaleway.dns = scaleway.dns
