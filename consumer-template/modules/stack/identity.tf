7cb71f29529e1b5d84f6ad15dfc3e3d5b390ece5
sheyaln <shehab70@gmail.com>
2026-05-25 19:32:49 -0400

    Bumped consumer-template refs to v3.1.4

--- consumer-template/modules/stack/identity.tf
+++ consumer-template/modules/stack/identity.tf
@@ -1,5 +1,5 @@
 module "identity" {
-  source = "git::https://github.com/sheyaln/sabokit.git//platform/identity/terraform?ref=v3.1.3"
+  source = "git::https://github.com/sheyaln/sabokit.git//platform/identity/terraform?ref=v3.1.4"
 
   gateway_domain = module.base.domains.gateway_domain
   base_domain    = module.base.domains.base_domain
