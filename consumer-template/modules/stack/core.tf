ed5bb11f8b6cabd8f70d15ed8163002ec7f27096
sheyaln <shehab70@gmail.com>
2026-05-28 12:17:58 -0400

    Bumped consumer-template refs to v3.5.7

--- consumer-template/modules/stack/core.tf
+++ consumer-template/modules/stack/core.tf
@@ -4,7 +4,7 @@
 # services off via var.core.<svc>.enabled = false.
 
 module "core" {
-  source = "git::https://github.com/sheyaln/sabokit.git//platform/core/terraform?ref=v3.5.6"
+  source = "git::https://github.com/sheyaln/sabokit.git//platform/core/terraform?ref=v3.5.7"
 
   base = local.base
 
