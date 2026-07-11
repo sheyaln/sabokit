f5f86ef663a7b08d5d0f05ddbc76eeac66b0bdd6
sheyaln <shehab70@gmail.com>
2026-07-11 13:15:58 -0400

    Bumped consumer-template refs to v0.2.3-beta1

--- consumer-template/environments/_template/infra/stack.tf
+++ consumer-template/environments/_template/infra/stack.tf
@@ -27,7 +27,7 @@ locals {
 }
 
 module "infra" {
-  source = "git::https://github.com/sheyaln/sabokit.git//platform/infra/terraform?ref=v0.2.2-beta1"
+  source = "git::https://github.com/sheyaln/sabokit.git//platform/infra/terraform?ref=v0.2.3-beta1"
   providers = {
     scaleway     = scaleway
     scaleway.dns = scaleway.dns
